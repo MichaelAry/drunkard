fdaad18e16ea9075ebcdfef3107b6d6c493f05b3
Michael <67581497+MichaelAry@users.noreply.github.com>
2025-02-25 02:07:28 +0300

    Delete renderer.js

--- renderer.js
+++
@@ -1,9 +0,0 @@
-function renderCurrentStep() {
-  gameField.innerHTML = `Ход игрока ${(step % 2) + 1}`;
-  gameField.innerHTML += `<br>Игрок 1 ${
-    gameDesks[0].length
-  }: ${gameDesks[0].map((el) => SUITS[el-1])}`;
-  gameField.innerHTML += `<br>Игрок 2 ${
-    gameDesks[1].length
-  }: ${gameDesks[1].map((el) => SUITS[el-1])}`;
-}
